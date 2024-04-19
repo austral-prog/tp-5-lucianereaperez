87356989889a368df04c7331d539e266713f4204
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-04-19 12:01:19 -0300

    Update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,17 +1,17 @@
 def max_of_two(x, y):
     if x <= y :
-        return y
-    else :
-        return x
-max_of_two(5,4)    
-max_of_two(-2,-3)
-max_of_two(0,0)
+       return y
+    else:
+       return x
 def max_of_three(x, y, z):
     if x <= y and z <= y :
-        return y
+       return y
     elif y <= z and x <= z :
-        return z
-    else :
-        return x
+       return z
+    else:
+       return x
+max_of_two(5,4)
+max_of_two(-2,-3)
+max_of_two(0,0)
 max_of_three(-2,-3,-1)
-max_of_three(0,0,0) 
+max_of_three(0,0,0)

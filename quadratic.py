c427dc38e32e7a60308c5d6b2518011a45a6a47d
lucianereaperez <162724754+lucianereaperez@users.noreply.github.com>
2024-04-19 22:49:17 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -5,7 +5,6 @@ def roots(a, b, c):
         r1= (-b + math.sqrt(discriminante))/(2*a)
         r2= (-b - math.sqrt(discriminante))/(2*a)
         return f"({r1}, {r2})"
-       
     elif discriminante == 0:
         return f"({-b/(2*a)})"
     else :
@@ -13,15 +12,25 @@ def roots(a, b, c):
 def value_y(a, b, c, x):
     y = a*(x**2)+(b*x)+c
     return y
+
 def to_string(a, b, c):
-    return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    if a!=0 and b!=0 and c!=0:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    elif a==0 and b!=0 and c!=0:
+        return f"f(x) = {b} * X + {c}"
+    elif a!=0 and b==0 and c!=0 :
+        return f"f(x) = {a} * X^2 + {c}"
+    elif a!=0 and b!=0 and c==0 :
+        return f"f(x) = {a} * X^2 + {b} * X"
+    elif a==0 and b == 0 and c!=0 :
+        return f"f(x) = {c}"
+
 def derivation(a, b, c):
-    return f"f'(x) = {2 * a}x + {b}"
-roots(1, -3, 2) 
-roots(1, -2, 1) 
-roots(1, 2, 3)  
-value_y(1, -3, 2, 0) 
-value_y(1, -3, 2, 1) 
-value_y(1, -3, 2, -1) 
-to_string(2, -3, 1) 
-derivation(2, -3, 1)
+    if a!=0 and b!=0:
+        return f"f'(x) = {2*a} * X + {b}"
+    elif a==0 and b!=0:
+        return f"f'(x) = {b}"
+    elif a!=0 and b==0:
+        return f"f'(x) = {2*a} * X"
+    elif a==0 and b==0:
+        return f"f'(x) = 0"
